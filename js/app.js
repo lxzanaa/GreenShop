feb4e90f9481bb526e0331b112a8cc325a05143a
lxzanaa <zari.weone2010@gmail.com>
2024-04-19 22:01:04 +0300

    section-sidebar tugadi

--- js/app.js
+++ js/app.js
@@ -8,3 +8,25 @@ headerContainerLink.forEach(function (item, index) {
         item.classList.add("active");
     })
 })
+
+let sectionSidebarItem = document.querySelectorAll(".section__sidebar-item")
+
+sectionSidebarItem.forEach(function (item, index) {
+    item.addEventListener('click', function () {
+        sectionSidebarItem.forEach(function (element) {
+            element.classList.remove("active")
+        })
+        item.classList.add("active");
+    })
+})
+
+let sectionSidebarItem2 = document.querySelectorAll(".section__sidebar-item2")
+
+sectionSidebarItem2.forEach(function (item, index) {
+    item.addEventListener('click', function () {
+        sectionSidebarItem2.forEach(function (element) {
+            element.classList.remove("green-active")
+        })
+        item.classList.add("green-active");
+    })
+})
